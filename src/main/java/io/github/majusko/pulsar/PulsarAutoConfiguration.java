d099fa327f4ac064939eb1a9533faa86bbe08afd
zhangzengmin <xiaomin0322@sina.com>
2020-06-15 18:50:20 +0800

    update

--- src/main/java/io/github/majusko/pulsar/PulsarAutoConfiguration.java
+++ src/main/java/io/github/majusko/pulsar/PulsarAutoConfiguration.java
@@ -1,6 +1,6 @@
 package io.github.majusko.pulsar;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.pulsar.client.api.AuthenticationFactory;
@@ -13,9 +13,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import io.github.majusko.pulsar.util.ConfigurationDataUtils;
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @ComponentScan
 @EnableConfigurationProperties(PulsarProperties.class)
+@Slf4j
 public class PulsarAutoConfiguration {
 
 	private final PulsarProperties pulsarProperties;
@@ -27,19 +31,16 @@ public class PulsarAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean
 	public PulsarClient pulsarClient() throws PulsarClientException {
-		ClientBuilder maxBackoffInterval = PulsarClient.builder().serviceUrl(pulsarProperties.getServiceUrl())
-				.ioThreads(pulsarProperties.getIoThreads()).listenerThreads(pulsarProperties.getListenerThreads())
-				.enableTcpNoDelay(pulsarProperties.isEnableTcpNoDelay())
-				.keepAliveInterval(pulsarProperties.getKeepAliveIntervalSec(), TimeUnit.SECONDS)
-				.connectionTimeout(pulsarProperties.getConnectionTimeoutSec(), TimeUnit.SECONDS)
-				.operationTimeout(pulsarProperties.getOperationTimeoutSec(), TimeUnit.SECONDS)
-				.startingBackoffInterval(pulsarProperties.getStartingBackoffIntervalMs(), TimeUnit.MILLISECONDS)
-				.maxBackoffInterval(pulsarProperties.getMaxBackoffIntervalSec(), TimeUnit.SECONDS);
-
+		if(pulsarProperties == null) {
+		   log.error("pulsarProperties is null");	
+		   return null;
+		}
+		ClientBuilder maxBackoffInterval = PulsarClient.builder();
+		Map<String, Object> map = ConfigurationDataUtils.toMap(pulsarProperties, PulsarProperties.class);
+		maxBackoffInterval.loadConf(map);
 		if (StringUtils.isNotBlank(pulsarProperties.getToken())) {
 			maxBackoffInterval.authentication(AuthenticationFactory.token(pulsarProperties.getToken()));
 		}
-
 		return maxBackoffInterval.build();
 
 	}
--- src/main/java/io/github/majusko/pulsar/PulsarProperties.java
+++ src/main/java/io/github/majusko/pulsar/PulsarProperties.java
@@ -3,6 +3,8 @@ package io.github.majusko.pulsar;
 import org.apache.pulsar.client.impl.conf.ClientConfigurationData;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,15 +16,7 @@ public class PulsarProperties extends ClientConfigurationData {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String serviceUrl = "pulsar://localhost:6650";
-	private Integer ioThreads = 10;
-	private Integer listenerThreads = 10;
-	private boolean enableTcpNoDelay = false;
-	private Integer keepAliveIntervalSec = 20;
-	private Integer connectionTimeoutSec = 10;
-	private Integer operationTimeoutSec = 15;
-	private Integer startingBackoffIntervalMs = 100;
-	private Integer maxBackoffIntervalSec = 10;
-	private String  token;
+	@JsonIgnore
+	private String token;
 
 }
