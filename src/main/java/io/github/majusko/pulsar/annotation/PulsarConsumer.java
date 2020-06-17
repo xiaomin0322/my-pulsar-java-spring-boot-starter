9ba2217493106717164457244195b6c345b64d0d
zhangzengmin <xiaomin0322@sina.com>
2020-06-17 15:28:22 +0800

    添加注释

--- src/main/java/io/github/majusko/pulsar/annotation/PulsarConsumer.java
+++ src/main/java/io/github/majusko/pulsar/annotation/PulsarConsumer.java
@@ -12,13 +12,32 @@ import org.apache.pulsar.client.api.SubscriptionType;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface PulsarConsumer {
+
+	/**
+	 * topic 名称 {persistent|non-persistent}://tenant/namespace/topic
+	 */
 	String topic();
 
+	/**
+	 * 发送消息类得Class对象
+	 */
 	Class<?> clazz();
 
+	/**
+	 * 序列化方式，默认JSON
+	 */
 	Serialization serialization() default Serialization.JSON;
 
+	/**
+	 * 订阅方式默认共享模式 Shared:共享模式不保证有序，如果对有序有要求使用failover
+	 * 更多参考：http://pulsar.apache.org/docs/en/concepts-messaging/#subscriptions
+	 */
 	SubscriptionType subscriptionType() default SubscriptionType.Shared;
 
+	/**
+	 * 自定义配置Class类，在该class中实现一个返回值为 ConsumerConfigurationDataExt对象
+	 * 更多参考：http://pulsar.apache.org/docs/en/concepts-messaging/#consumers
+	 * 
+	 */
 	Class<?>[] configuration() default {};
 }
