1ab73da73a21c83b781d206ee50838b2d14bbb3b
zhangzengmin <xiaomin0322@sina.com>
2020-06-17 18:50:21 +0800

    123

--- src/test/java/io/github/majusko/pulsar/TestProducerConfiguration.java
+++ src/test/java/io/github/majusko/pulsar/TestProducerConfiguration.java
@@ -1,8 +1,8 @@
 package io.github.majusko.pulsar;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.github.majusko.pulsar.constant.Serialization;
 import io.github.majusko.pulsar.producer.ProducerFactory;
 import io.github.majusko.pulsar.producer.ProducerHolder;
 
@@ -11,11 +11,9 @@ public class TestProducerConfiguration {
 
 	//@Bean
 	public ProducerFactory producerFactory() {
-
 		ProducerHolder configurationDataExt = new ProducerHolder();
 		configurationDataExt.setTopic("topic-zzm");
 		configurationDataExt.setClazz(MyMsg2.class);
-		return new ProducerFactory().addProducer("topic-one", MyMsg.class)
-				.addProducer("topic-two", MyMsg2.class, Serialization.JSON).addProducer("topic-zzm", MyMsg2.class);
+		return new ProducerFactory().addProducer(configurationDataExt);
 	}
 }
